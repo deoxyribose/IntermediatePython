e34791c0fbc1bbdd4428530c59efd9fa31f6d141
frans <frzdyb@gmail.com>
2010-10-23 16:31:38 +0200

    A program that generates valid cpr-numbers given a gender and birthdate

--- tjekcpr.py
+++ tjekcpr.py
@@ -4,9 +4,9 @@ def tjekcpr(n):
     weight = [4,3,2,7,6,5,4,3,2,1]
     kontrol = [int(n[i])*weight[i] for i in range(len(weight))]
     return sum(kontrol) % 11
-r = tjekcpr(input("Skriv cpr-nummeret: "))
-if r == 0:
-    print("Det angivne cpr-nummer er gyldigt.")
-else:
-    print("Det angivne cpr-nummer er ikke gyldigt.")
-
+if __name__ == '__main__':
+    r = tjekcpr(input("Skriv cpr-nummeret: "))
+    if r == 0:
+        print("Det angivne cpr-nummer er gyldigt.")
+    else:
+        print("Det angivne cpr-nummer er ikke gyldigt.")
