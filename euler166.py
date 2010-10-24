78ed00833299e7845a01a5720af3ede3e4756b1f
frans <frzdyb@gmail.com>
2010-10-24 14:52:52 +0200

    euler166 update

--- euler166.py
+++ euler166.py
@@ -1,7 +1,6 @@
 # Problem: Resulting matrices are not magic squares.
 from numpy import array, zeros 
 from numpy.linalg import solve
-d = [0,1,2,3,4,5,6,7,8,9]
 m = zeros((4,4))
 count = 0
 r1 = range(10)
@@ -30,8 +29,11 @@ def calc_lastpiece():
     matrix = array([1,1,0,0,1,0,0,1,1,0,1,0,0,0,1,1]).reshape(4,4)
     vektor = array([summen - (m[2,0]+m[2,1]).sum(), summen - (m[0,0]+m[1,1].sum()), summen - (m[0,2]+m[1,2].sum()), summen - (m[0,3]+m[1,3].sum())])
     return solve(matrix,vektor).reshape(2,2)
-for i in range(10**4):
-    print("##########NY ITERATION#########")
+#
+#
+#
+for i in xrange(10**10):
+#    print("##########NY ITERATION#########")
     m[0,0:] = gen_firstpiece(i)
 # summen er det tal som alle raekker, soejler og diagonaler summerer op til.
     summen = sum(m[0,0:])
@@ -45,12 +47,15 @@ for i in range(10**4):
         for k in r2:
             p3 = gen_secondpiece(k)
             if m[0,1].sum() + p3.sum() <= summen and len([calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])==4:
-                print('calc_lastpiece er: ', [calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])
+                #print('calc_lastpiece er: ', [calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])
                 m[1,1:3] = p3
                 m[1,3] = summen - m[1,0:3].sum()
-                m[2,1] = summen - (m[0,3] + m[1,2] + m[3,0])
+                m[2,1] = summen - (m[0,3] + m[1,2] + m[3,0]).sum()
                 m[3,1] = summen - m[0:3,1].sum()
                 m[2:,2:] = calc_lastpiece()
+                if i > 10:
+                    print(m)
+                """
                 print(m)
                 diag1 = []
                 diag2 = []
@@ -59,4 +64,4 @@ for i in range(10**4):
                     diag1 = sum([m[i,i] for i in range(3)])
                     diag2 = sum([m[i,j] for i in range(3) for j in range(3) if i+j == 3])
                 print("Summen af forste diagonal er: ", diag1,"Summen af anden diagonal er: ", diag2)
-                print()
+                """
