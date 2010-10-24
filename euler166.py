d57d68e459f6d8d77fd4e05df7e41ba79d45bc2c
frans <frzdyb@gmail.com>
2010-10-24 14:06:56 +0200

    update

--- euler166.py
+++ euler166.py
@@ -1,5 +1,5 @@
-# Resulting matrices are not magic squares.
-from numpy import array, zeros
+# Problem: Resulting matrices are not magic squares.
+from numpy import array, zeros 
 from numpy.linalg import solve
 d = [0,1,2,3,4,5,6,7,8,9]
 m = zeros((4,4))
@@ -15,15 +15,13 @@ for a in r1:
                 firstpiece.append(b)
                 firstpiece.append(c)
                 firstpiece.append(d)
-print(len(firstpiece))
 def gen_firstpiece(n):
-    return array(firstpiece[n:n+4])
+   return array(firstpiece[n:n+4])
 secondpiece = []
 for a in r1:
     for b in r1:
         secondpiece.append(a)
         secondpiece.append(b)
-print(len(secondpiece))
 def gen_secondpiece(n):
 # En funktion som genererer den de to brikker med hver to tal. 
     return array(secondpiece[n:n+2])
@@ -33,6 +31,7 @@ def calc_lastpiece():
     vektor = array([summen - (m[2,0]+m[2,1]).sum(), summen - (m[0,0]+m[1,1].sum()), summen - (m[0,2]+m[1,2].sum()), summen - (m[0,3]+m[1,3].sum())])
     return solve(matrix,vektor).reshape(2,2)
 for i in range(10**4):
+    print("##########NY ITERATION#########")
     m[0,0:] = gen_firstpiece(i)
 # summen er det tal som alle raekker, soejler og diagonaler summerer op til.
     summen = sum(m[0,0:])
@@ -45,11 +44,19 @@ for i in range(10**4):
             break
         for k in r2:
             p3 = gen_secondpiece(k)
-            if m[0,1].sum() + p3.sum() <= summen:
+            if m[0,1].sum() + p3.sum() <= summen and len([calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])==4:
+                print('calc_lastpiece er: ', [calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])
                 m[1,1:3] = p3
-                m[1,3] = summen - sum(m[1,0:3])
+                m[1,3] = summen - m[1,0:3].sum()
                 m[2,1] = summen - (m[0,3] + m[1,2] + m[3,0])
-                m[3,1] = summen - sum(m[1,0:3])
+                m[3,1] = summen - m[0:3,1].sum()
                 m[2:,2:] = calc_lastpiece()
                 print(m)
-
+                diag1 = []
+                diag2 = []
+                for i in range(3):
+                    print("Summen af %i'te raekke er " % i, m[i,:].sum(), "Summen af %i'te soejle er " % i, m[:,i].sum()) 
+                    diag1 = sum([m[i,i] for i in range(3)])
+                    diag2 = sum([m[i,j] for i in range(3) for j in range(3) if i+j == 3])
+                print("Summen af forste diagonal er: ", diag1,"Summen af anden diagonal er: ", diag2)
+                print()
