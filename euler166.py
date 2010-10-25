0788588055caecc3dcde2d4b53ffd84f55b21302
frans <frzdyb@gmail.com>
2010-10-25 13:21:08 +0200

    Update euler166)

--- euler166.py
+++ euler166.py
@@ -1,5 +1,6 @@
 # Problem: Resulting matrices are not magic squares.
 from numpy import array, zeros 
+from math import isnan
 from numpy.linalg import solve
 m = zeros((4,4))
 count = 0
@@ -28,31 +29,39 @@ def calc_lastpiece():
 # Beregn de sidste fire tal.
     matrix = array([1,1,0,0,1,0,0,1,1,0,1,0,0,0,1,1]).reshape(4,4)
     vektor = array([summen - (m[2,0]+m[2,1]).sum(), summen - (m[0,0]+m[1,1].sum()), summen - (m[0,2]+m[1,2].sum()), summen - (m[0,3]+m[1,3].sum())])
-    return solve(matrix,vektor).reshape(2,2)
+    x = solve(matrix,vektor).reshape(2,2)
+    if len([x.flatten(1)[i] for i in range(4) if x.flatten(1)[i]==abs(round(x.flatten(1)[i]))])==4:
+        return x
+    else:
+        return False
+        #Returnerer kun nul-matricer...!!!!
 #
 #
-#
-for i in xrange(10**10):
+for i in xrange(10**4):
 #    print("##########NY ITERATION#########")
     m[0,0:] = gen_firstpiece(i)
 # summen er det tal som alle raekker, soejler og diagonaler summerer op til.
     summen = sum(m[0,0:])
-    for j in r2:
+    for j in range(100):
         p2 = gen_secondpiece(j)
         if m[0,0].sum() + p2.sum() <= summen:
             m[1:3,0] = p2
             m[3,0] = summen - sum(m[0:3,0])
         else:
             break
-        for k in r2:
+        for k in range(100):
             p3 = gen_secondpiece(k)
-            if m[0,1].sum() + p3.sum() <= summen and len([calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])==4:
-                #print('calc_lastpiece er: ', [calc_lastpiece().flatten(1)[x] for x in range(len(calc_lastpiece().flatten(1))) if calc_lastpiece().flatten(1)[x]==abs(int(calc_lastpiece().flatten(1)[x]))])
+#            print(calc_lastpiece())
+            if m[0,1].sum() + p3.sum() <= summen:
                 m[1,1:3] = p3
                 m[1,3] = summen - m[1,0:3].sum()
+                if m[1,3] <= 0: break                    
                 m[2,1] = summen - (m[0,3] + m[1,2] + m[3,0]).sum()
+                if m[2,1] <= 0: break                    
                 m[3,1] = summen - m[0:3,1].sum()
+                if m[3,1] <= 0: break                    
                 m[2:,2:] = calc_lastpiece()
+                if any([any(map(isnan,r)) for r in m[2:,2:].reshape(4,1)]): break
                 if i > 10:
                     print(m)
                 """
